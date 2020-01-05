127310c95122bd613e121f879f9cf76604b579c8
Henrique Luz <riqueluz@gmail.com>
2020-01-05 17:42:02 +0100

    Removing useless code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          Hello!
         </p>
         <a
           className="App-link"
